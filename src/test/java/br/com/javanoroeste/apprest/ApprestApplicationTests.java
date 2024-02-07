bf909ef78194bb2d2d8e8a1721cf75a4c54d17d3
carlos <carlosdev65@gmail.com>
2024-02-07 09:37:56 -0300

    Projeto inicial!

---
+++ src/test/java/br/com/javanoroeste/apprest/ApprestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javanoroeste.apprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
